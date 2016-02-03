c0312718b6009ca690aa847bc277bca08b50efe6
Bobillier Michel <>
2016-02-03 17:42:50 +0100

    dev

--- common/script/php52.sh
+++ common/script/php52.sh
@@ -11,8 +11,10 @@ echo '$$$ install apache2'
 sudo apt-get install -y apache2 apache2-mpm-prefork
 echo '$$$ install php 5.2'
 sudo apt-get install -y php52-cli libapache2-mod-php52 php52-mod-curl php52-mod-gettext php52-mod-json php52-mod-ldap php52-mod-mbstring php52-mod-mcrypt php52-mod-mysql php52-mod-soap php52-mod-simplexml
-sudo apt-get install -y php52-dev php52-pear php52-pecl
+sudo apt-get install -y php52-dev php52-pear php52-pecl php52-mod-xcache
 sudo apt-get install -y php52-mod-xdebug
+sudo ln -s /etc/php52/conf.d/50-xcache.ini /etc/php52/apache2/conf.d/50-xcache.ini
+sudo ln -s /etc/php52/conf.d/50-xcache.ini /etc/php52/cli/conf.d/50-xcache.ini
 sudo ln -s /etc/php52/conf.d/xdebug.ini /etc/php52/apache2/conf.d/xdebug.ini
 sudo a2enmod rewrite
 sudo a2enmod alias
